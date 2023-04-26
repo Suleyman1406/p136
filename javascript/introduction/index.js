e1d8832cb9ffcf011b381902e341d7bd0bbffbe0
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-04-26 12:43:09 +0400

    js lessons added

---
+++ javascript/introduction/index.js
@@ -0,0 +1,75 @@
+
+// console.log("Salam2");
+
+// conso.log("Salam");
+
+// console.log('Test');
+
+// console.log("res");
+
+/*
+
+  This is multiple comment block
+
+*/
+
+// int a =4;
+// string surname = "Hecer";
+// bool isHecer = true;
+// double hecersAge = 12.0;
+// int[] myArr = new int[5]; 
+// a = "sad"
+// let var const
+
+// let a = 4;
+// a=true;
+// let surname = "hecer";
+// let isHecer = true;
+// let whatHecerDoing = 'Always trash talking';
+// let hecersAge = 12.0;
+// let myArray = [1,2,3,4,5, true, 'hecer',"ilkin",1.2]
+// myArray[81]= "Salam";
+// myArray.push('12')
+// console.log(myArray);
+
+// var a = 4;
+// a=true;
+// var surname = "hecer";
+// var isHecer = true;
+// var whatHecerDoing = 'Always trash talking';
+// var hecersAge = 12.0;
+// var myArray = [1,2,3,4,5, true, 'hecer',"ilkin",1.2]
+// myArray[81]= "Salam";
+// myArray.push('12')
+// console.log(myArray);
+
+// let a;
+// a=4;
+
+// console.log(a);
+
+// const myName = "Suleyman";
+// const myNAme = "Suleyman";
+// const my_Name = "Suleyman";
+// const my-Name = "Suleyman";
+// const MY_NAME = "Suleyman";
+// let $salam = "test";
+// let _salam = "test";
+// let 1-salam = "test";
+// let for = "asd";
+// let surname = "asd";
+// surname=null;
+// console.log(surname);
+
+// console.log(-1/0);
+
+// console.log('Ceyhun');
+// console.log("Ceyhun");
+// console.log(`Ceyh
+
+// un`);
+
+let a ="Suleyman";
+let b ="Dadashow";
+console.log(`${a} ${b}`);
+
