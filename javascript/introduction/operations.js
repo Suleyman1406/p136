acc63bac0fad92b320d60394831a53d0122b7720
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-04-27 12:51:34 +0400

    feat: js lessons added

---
+++ javascript/introduction/operations.js
@@ -0,0 +1,171 @@
+// console.log(2+4);
+// console.log(2-4);
+// console.log(2*4);
+// console.log(1/7);
+// console.log(19%5);
+// console.log(2**3);
+// console.log("test","gasdas",123);
+
+// let a = 5;
+// a++;
+// a--;
+// a-=2;
+// a/=2;
+// a*=3;
+// console.log(a);
+
+// let a = 5;
+// console.log(a++, a);
+// console.log(a--, a);
+// console.log(--a, a);
+// console.log(++a, a);
+
+// a 5 4 5
+// b 8 9 10
+// c 18
+// let a = 5;
+// let b = --a + a++; // 4 + 4
+// let c = b++ + ++b; // 8 + 10
+// console.log(`${a} ${b} ${c}`);
+// Fexri, Zeyqem, Fidan 5 9 19
+// Isa 5 45 45
+// Nicat, Denziyev 5 8 18
+
+// a 3 2 3 2 3 2 3 2
+// b 2 1 2 3 2
+// c 2 1
+// d 4
+
+// let a = 3;
+// let b = --a + a++ - --a; // 2 + 2 - 2
+// let c = a++ - --b + ++b + b++ - a--; // 2 - 1 + 2 + 2 -3
+// let d = a++ - --b + --c + a--; // 2 - 2 + 1 + 3
+// console.log(`${a} ${b} ${c} ${d}`);
+
+// console.log(2>5);
+// console.log(2<5);
+// console.log(2<=5);
+// console.log(2>=2); // > =
+// = =  = = =
+// console.log(2 == 2); //  ==
+// console.log(2 == "2"); // ===
+
+// console.log(2 === "3");
+// console.log(2 === "2");
+// console.log("salam" == "2");
+
+// console.log(2 != 2); // false
+// console.log(2 != 3); // true
+// console.log(2 !== '2'); // ! == true
+// console.log( 2 !== 2); // false
+// console.log("salam" !== "Salam"); // true
+
+// console.log(true && true); // true
+// console.log((true && false && false) || (true || true)); // false
+// console.log((a>b && a==1) || (a<b && a==2));
+
+// console.log(true || false);
+// console.log(false || false);
+
+// console.log(false || true  );
+
+// console.log(true && 'salam' && '' || "test" || 'nicat' || "fexri");
+
+// console.log(null || 'necesen' && 'test' || 'Isa');
+// console.log(null || 'necesen' || undefined && 'test');
+// let a =5;
+// if(a>3){
+//   console.log('Hecer');
+// }else if(a<3){
+//   console.log('Nail');
+// }else{
+//   console.log('');
+// }
+
+// let myCommand = "Stop"
+// switch(myCommand){
+//   case "Stop":
+//     console.log('Stopped');
+//     break;
+//   case "Start":
+//     console.log('Started');
+//     break;
+//   default:
+//     console.log("Həcər is always talking");
+// }
+
+// for (let i = 0, j = 2; i <= 10 || j <= 5; i++, j++) {
+//   console.log(i, j);
+// }
+// for (;;) {
+//   if (2 == 2) break;
+//   console.log("test");
+// }
+// let a
+// while()
+
+// for (let i = 0; i < 10; i++) {
+//   if (i == 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+// let a = 0;
+// while (a <= Infinity) {
+//   console.log(a);
+//   a++;
+// }
+
+// let a = 0;
+// do {
+//   console.log(++a);
+// } while (false);
+
+// let command = "diger";
+// if (command === "start") {
+//   console.log("starting");
+// } else if (command === "stop") {
+//   console.log("stopping");
+// } else if (command === "wait") {
+//   console.log("waiting");
+// } else {
+//   console.log("Command not found");
+// }
+
+// let value = Boolean(0);
+
+// if (value === true) {
+//   console.log("Correct");
+// } else {
+//   console.log("Wrong");
+// }
+
+let a = true;
+
+// if (!(2 > 3 && 4 > 2)) {
+//   console.log("Test");
+// } else {
+//   console.log("false");
+// }
+
+// if () {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// let value = 4;
+// let result = ""; //12
+
+// for (let i = 1; i <= value; i++) {
+//   result += i;
+//   console.log(result);
+// }
+
+// for (let i = 1; i <= value; i++) {
+//   let res = "";
+//   for (let j = 1; j <= i; j++) {
+//     res += j;
+//   }
+//   console.log(res);
+// }
---
+++ javascript/introduction/type.js
@@ -0,0 +1,28 @@
+// console.log(typeof `tyasd`);
+// console.log(typeof 12);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+
+// console.log(typeof String(12));
+// console.log( String(true));
+// console.log( String(12.2));
+// console.log(true+" "+1);
+// console.log(1+1+"2");
+// console.log(1+1+22+" "+1+1+2);
+
+// console.log(Number("12"));
+// console.log(Number(false));
+// console.log(Number("salam"));
+// console.log( +"20");
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+// console.log(Boolean(-44));
+// console.log(Boolean("Jesus"));
+// console.log(Boolean(""));
+// console.log(Boolean(" "));
+// console.log(Boolean(+"0"));
+console.log();
+
