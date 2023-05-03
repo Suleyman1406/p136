2a676c62314205abcbcef5a7cf5d93bc512477c4
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-05-03 10:19:14 +0400

    some exercises added

--- javascript/assignments/index.js
+++ javascript/assignments/index.js
@@ -79,3 +79,37 @@
 //   return myArr;
 // }
 // console.log(swapPairs([1, 2, 3, 4, 5, 6]));
+
+// console.log(undefined || "" && "test");
+
+// console.log("test" !== 'test'); // false
+// console.log(5 != '5');// false
+
+// function calculator(a,b){
+//   if(a==5){
+//     b();
+//   }
+// }
+
+// calculator(5, function(){
+//   console.log('test');
+// })
+
+// console.log(Number.isNaN());
+
+// console.log();
+
+// console.log(parseInt('100.5px'));
+// console.log(parseFloat('s100.5px'));
+// console.log(Number('100px'));
+
+// let myArr = [1,2,3,2,4];
+
+// console.log(myArr.splice(1,3));
+
+// console.log(myArr);
+
+// console.log(myArr.join('-').split(''));
+            //  "1-2-3-4" -> ['1','2','3','4']
+
+// console.log(Math.random());
