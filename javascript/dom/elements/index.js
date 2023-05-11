3fe3ac8b97b096caee44f7446debe873186d6fff
suleyman1406 <dadasovsuleyman126@gmail.com>
2023-05-11 09:56:06 +0400

    elements lesson added

---
+++ javascript/dom/elements/index.js
@@ -0,0 +1,11 @@
+// console.log(document.body.childElementCount);
+// console.log(document.body.children);
+// console.log(document.body.childNodes);
+const firstDivInTheWorld = document.getElementById("firstDivInTheWorld");
+// console.log(firstDivInTheWorld.parentElement.parentElement.firstElementChild);
+// console.log(
+//   firstDivInTheWorld.parentElement.parentElement.lastElementChild
+//     .lastElementChild
+// );
+// console.log(firstDivInTheWorld.previousElementSibling);
+// firstDivInTheWorld.nextElementSibling.nextElementSibling.remove();
